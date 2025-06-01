67b60648c3b365345dde269584eb9e0e5c5f4663
MuxiddinovaS <solihamuhiddinova30@mail.ru>
2025-06-01 22:23:29 +0500

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,61 @@
+
+ #Given a side of square. Find its perimeter and area
+
+# Ввод длины стороны квадрата
+#side = float(input("Введите длину стороны квадрата: "))
+
+# Вычисление периметра и площади
+#perimeter = 4 * side
+#area = side ** 2
+
+# Вывод результатов
+#print("Периметр квадрата:", perimeter)
+#print("Площадь квадрата:", area)
+
+#Given diameter of circle. Find its length.
+
+
+#import math  # импортируем модуль math, чтобы использовать число π (pi)
+
+# Ввод диаметра от пользователя
+#diameter = float(input("Введите диаметр круга: "))
+
+# Вычисление длины окружности
+#length = math.pi * diameter
+
+# Вывод результата
+#print("Длина окружности:", round(length, 2))
+
+
+
+#Given two numbers a and b. Find their mean.
+
+# Ввод двух чисел от пользователя
+#a = float(input("Введите первое число (a): "))
+#b = float(input("Введите второе число (b): "))
+
+# Вычисление среднего
+#mean = (a + b) / 2
+
+# Вывод результата
+#print("Среднее значение:", mean)
+
+
+
+#Given two numbers a and b. Find their sum, product and square of each number.
+# Ввод двух чисел
+#a = float(input("Введите первое число (a): "))
+#b = float(input("Введите второе число (b): "))
+
+# Вычисления
+#summa = a + b
+#product = a * b
+#square_a = a ** 2
+#square_b = b ** 2
+
+# Вывод результатов
+#print("Сумма:", summa)
+#print("Произведение:", product)
+#print("Квадрат числа a:", square_a)
+#print("Квадрат числа b:", square_b)
+
